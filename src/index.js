5116a591e8a06c87765e7a42986343055ba4d535
czx <chenzhixiang@yit.com>
2019-08-06 16:13:39 +0800

    store

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/style/index.css';
 import App from './views/App';
+import store from './store';
 
 ReactDOM.render(<App />, document.getElementById('root'));
